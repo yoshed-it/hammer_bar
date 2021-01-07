0058e84528f41e887aa4435820266ca992b063cc
Yosh <sauteboy@gmail.com>
2021-01-07 14:41:49 -0800

    new branch added behav

---
+++ Assets/Scripts/AI/TestAI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class TestAI : MonoBehaviour
+{
+    public bool CheckState;
+    public  bool Drunk;
+    public  int Strength;
+
+    public void RandomizeStats()
+    {
+        if (Random.Range(0, 10) < 5)
+        {
+            Drunk = true;
+        }
+        else
+        {
+            Drunk = false;
+        } 
+        Strength = Random.Range(0, 10);
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RandomizeStats();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
